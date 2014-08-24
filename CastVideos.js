87f4bab9ccb224ba3bf7f1160d7095859f0623d3
Louise Newberry <nn6215@googlemail.com>
2014-08-25 00:20:30 +0100

    Change video thumbnails to text links and only show the title of the videos.

--- CastVideos.js
+++ CastVideos.js
@@ -19,12 +19,12 @@
 /**
  * Media source root URL
  **/
-var MEDIA_SOURCE_ROOT = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/';
+var MEDIA_SOURCE_ROOT = 'http://192.168.0.79/video/videos/';
 
 /**
  * Media source URL JSON
  **/
-var MEDIA_SOURCE_URL = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/videos.json';
+var MEDIA_SOURCE_URL = 'http://192.168.0.79/video/videos/videos.json';
 
 /**
  * Width of progress bar in pixel
@@ -140,8 +140,7 @@ CastPlayer.prototype.initializeCastPlayer = function() {
   }
   // default set to the default media receiver app ID
   // optional: you may change it to point to your own
-  //var applicationID = chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID;
-  var applicationID = '4F8B3483';
+  var applicationID = chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID;
 
   // auto join policy can be one of the following three
   var autoJoinPolicy = chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED;
@@ -379,9 +378,6 @@ CastPlayer.prototype.loadMedia = function(mediaIndex) {
     this.onLoadMediaError.bind(this));
 
   document.getElementById("media_title").innerHTML = this.mediaContents[this.currentMediaIndex]['title'];
-  document.getElementById("media_subtitle").innerHTML = this.mediaContents[this.currentMediaIndex]['subtitle'];
-  document.getElementById("media_desc").innerHTML = this.mediaContents[this.currentMediaIndex]['description'];
-
 };
 
 /**
@@ -908,8 +904,6 @@ CastPlayer.prototype.selectMediaUpdateUI = function(mediaIndex) {
   document.getElementById('video_image').src = MEDIA_SOURCE_ROOT + this.mediaContents[mediaIndex]['thumb'];
   document.getElementById("progress").style.width = '0px';
   document.getElementById("media_title").innerHTML = this.mediaContents[mediaIndex]['title'];
-  document.getElementById("media_subtitle").innerHTML = this.mediaContents[mediaIndex]['subtitle'];
-  document.getElementById("media_desc").innerHTML = this.mediaContents[mediaIndex]['description'];
 };
 
 /**
@@ -918,8 +912,6 @@ CastPlayer.prototype.selectMediaUpdateUI = function(mediaIndex) {
 CastPlayer.prototype.initializeUI = function() {
   // set initial values for title, subtitle, and description 
   document.getElementById("media_title").innerHTML = this.mediaContents[0]['title'];
-  document.getElementById("media_subtitle").innerHTML = this.mediaContents[this.currentMediaIndex]['subtitle'];
-  document.getElementById("media_desc").innerHTML = this.mediaContents[this.currentMediaIndex]['description'];
 
   // add event handlers to UI components
   document.getElementById("casticonidle").addEventListener('click', this.launchApp.bind(this));
@@ -1096,7 +1088,7 @@ CastPlayer.prototype.addVideoThumbs = function() {
     divIdName = 'thumb'+i+'Div';
     newdiv.setAttribute('id',divIdName);
     newdiv.setAttribute('class','thumb');
-    newdiv.innerHTML = '<img src="' + MEDIA_SOURCE_ROOT + this.mediaContents[i]['thumb'] + '" class="thumbnail">';
+    newdiv.innerHTML = '<a href="#">' + this.mediaContents[i]['title'] +' </a>';
     newdiv.addEventListener('click', this.selectMedia.bind(this, i));
     ni.appendChild(newdiv);
   }
@@ -1107,83 +1099,29 @@ CastPlayer.prototype.addVideoThumbs = function() {
  */
 var mediaJSON = { "categories" : [ { "name" : "Movies",
         "videos" : [ 
-		    { "description" : "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
-              "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" ],
-              "subtitle" : "By Blender Foundation",
-              "thumb" : "images/BigBuckBunny.jpg",
-              "title" : "Big Buck Bunny"
+			{ "description" : "Finn (voiced by Jeremy Shada) and Jake (voiced by John DiMaggio) meet the <q>evil<q> Cute King (voiced by Jackie Buscarino) and the Cuties, who want to kill Finn and Jake. When they discover the Cuties have never won a fight, the two decide to fake their deaths in battle out of pity.",
+              "sources" : [ "/video/videos/ADVENTURE_TIME_SEASON_3_1.mkv" ],
+              "subtitle" : "Adventure Time season 3",
+              "thumb" : "BMO_AT_season_3.jpeg",
+              "title" : "Conquest of Cuteness"
             },
-            { "description" : "The first Blender Open Movie from 2006",
-              "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" ],
-              "subtitle" : "By Blender Foundation",
-              "thumb" : "images/ElephantsDream.jpg",
-              "title" : "Elephant Dream"
+			{ "description" : "Finn and Jake become trapped in an arena of gladiator ghosts. Finn gives in to bloodlust, and Jake becomes obsessed with keeping up the morale.",
+              "sources" : [ "/video/videos/ADVENTURE_TIME_SEASON_3_2.mkv" ],
+              "subtitle" : "Adventure Time season 3",
+              "thumb" : "BMO_AT_season_3.jpeg",
+              "title" : "Morituri Te Salutamus"
             },
-            { "description" : "HBO GO now works with Chromecast -- the easiest way to enjoy online video on your TV. For when you want to settle into your Iron Throne to watch the latest episodes. For $35.\nLearn how to use Chromecast with HBO GO and more at google.com/chromecast.",
-              "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4" ],
-              "subtitle" : "By Google",
-              "thumb" : "images/ForBiggerBlazes.jpg",
-              "title" : "For Bigger Blazes"
+			{ "description" : "When Marceline (voiced by Olivia Olson) accidentally traps herself in a sleep spell, Finn and Jake venture into her mind to save her. They soon learn that it was an elaborate trick by Marceline's ex-boyfriend Ash (voiced by Steve Agee) to win her back.",
+              "sources" : [ "/video/videos/ADVENTURE_TIME_SEASON_3_3.mkv" ],
+              "subtitle" : "Adventure Time season 3",
+              "thumb" : "BMO_AT_season_3.jpeg",
+              "title" : "Memory of a Memory"
             },
-            { "description" : "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when Batman's escapes aren't quite big enough. For $35. Learn how to use Chromecast with Google Play Movies and more at google.com/chromecast.",
-              "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4" ],
-              "subtitle" : "By Google",
-              "thumb" : "images/ForBiggerEscapes.jpg",
-              "title" : "For Bigger Escape"
-            },
-            { "description" : "Introducing Chromecast. The easiest way to enjoy online video and music on your TV. For $35.  Find out more at google.com/chromecast.",
-              "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4" ],
-              "subtitle" : "By Google",
-              "thumb" : "images/ForBiggerFun.jpg",
-              "title" : "For Bigger Fun"
-            },
-            { "description" : "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for the times that call for bigger joyrides. For $35. Learn how to use Chromecast with YouTube and more at google.com/chromecast.",
-              "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4" ],
-              "subtitle" : "By Google",
-              "thumb" : "images/ForBiggerJoyrides.jpg",
-              "title" : "For Bigger Joyrides"
-            },
-            { "description" :"Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when you want to make Buster's big meltdowns even bigger. For $35. Learn how to use Chromecast with Netflix and more at google.com/chromecast.", 
-              "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4" ],
-              "subtitle" : "By Google",
-              "thumb" : "images/ForBiggerMeltdowns.jpg",
-              "title" : "For Bigger Meltdowns"
-            },
-			{ "description" : "Sintel is an independently produced short film, initiated by the Blender Foundation as a means to further improve and validate the free/open source 3D creation suite Blender. With initial funding provided by 1000s of donations via the internet community, it has again proven to be a viable development model for both open 3D technology as for independent animation film.\nThis 15 minute film has been realized in the studio of the Amsterdam Blender Institute, by an international team of artists and developers. In addition to that, several crucial technical and creative targets have been realized online, by developers and artists and teams all over the world.\nwww.sintel.org",
-              "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4" ],
-              "subtitle" : "By Blender Foundation",
-              "thumb" : "images/Sintel.jpg",
-              "title" : "Sintel"
-            },
-			{ "description" : "Smoking Tire takes the all-new Subaru Outback to the highest point we can find in hopes our customer-appreciation Balloon Launch will get some free T-shirts into the hands of our viewers.",
-              "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4" ],
-              "subtitle" : "By Garage419",
-              "thumb" : "images/SubaruOutbackOnStreetAndDirt.jpg",
-              "title" : "Subaru Outback On Street And Dirt"
-            },
-			{ "description" : "Tears of Steel was realized with crowd-funding by users of the open source 3D creation tool Blender. Target was to improve and test a complete open and free pipeline for visual effects in film - and to make a compelling sci-fi film in Amsterdam, the Netherlands.  The film itself, and all raw material used for making it, have been released under the Creatieve Commons 3.0 Attribution license. Visit the tearsofsteel.org website to find out more about this, or to purchase the 4-DVD box with a lot of extras.  (CC) Blender Foundation - http://www.tearsofsteel.org", 
-              "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4" ],
-              "subtitle" : "By Blender Foundation",
-              "thumb" : "images/TearsOfSteel.jpg",
-              "title" : "Tears of Steel"
-            },
-			{ "description" : "The Smoking Tire heads out to Adams Motorsports Park in Riverside, CA to test the most requested car of 2010, the Volkswagen GTI. Will it beat the Mazdaspeed3's standard-setting lap time? Watch and see...",
-              "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4" ],
-              "subtitle" : "By Garage419",
-              "thumb" : "images/VolkswagenGTIReview.jpg",
-              "title" : "Volkswagen GTI Review"
-            },
-			{ "description" : "The Smoking Tire is going on the 2010 Bullrun Live Rally in a 2011 Shelby GT500, and posting a video from the road every single day! The only place to watch them is by subscribing to The Smoking Tire or watching at BlackMagicShine.com",
-              "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4" ],
-              "subtitle" : "By Garage419",
-              "thumb" : "images/WeAreGoingOnBullrun.jpg",
-              "title" : "We Are Going On Bullrun"
-            },
-			{ "description" : "The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.",
-              "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4" ],
-              "subtitle" : "By Garage419",
-              "thumb" : "images/WhatCarCanYouGetForAGrand.jpg",
-              "title" : "What care can you get for a grand?"
+			{ "description" : "",
+              "sources" : [ "http://127.0.0.1:8081/" ],
+              "subtitle" : "Adventure Time season 3",
+              "thumb" : "BMO_AT_season_3.jpeg",
+              "title" : ""
             }
     ]}]};
 
