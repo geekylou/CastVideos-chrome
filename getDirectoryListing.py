4b8b24939fe31241dfb9f1358cb9e29bd99e0c8c
Louise Newberry <nn6215@googlemail.com>
2014-08-25 11:57:46 +0100

    Add directory listing support to directory list server side script.

--- getDirectoryListing.py
+++ getDirectoryListing.py
@@ -1,25 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-# enable debugging
-from os import walk
+
+# The base path where your content is.  This should be set to '.' if content is in the same directory as the script.
+BASE_PATH="../videos"
+
+import os
 import cgitb
+import cgi
 import json
 cgitb.enable()
 
 print("Content-Type: application/json")
 print("")
     
-# The base path where your content is.
-BASE_PATH="../videos"
+arguments = cgi.FieldStorage()
 
+directory_arg = arguments.getvalue("directory")
+
+if directory_arg and directory_arg.find("..") > 0:
+    directory_arg = None
+
+# Guard against the script being used to get directory listings of directories below the base.
+if directory_arg:
+    path = BASE_PATH + "/" + directory_arg
+else:
+    path = BASE_PATH
+    
 videos = []
+directories = []
 
-for (dirpath, dirnames, filenames) in walk(BASE_PATH):    
+for (dirpath, dirnames, filenames) in os.walk(path):
+    if directory_arg:
+        if directory_arg.rfind('/') > 0:
+            directories.append( {"title" : ".." , "directory" : directory_arg[:directory_arg.rfind('/')] } )
+        else:
+            directories.append( {"title" : ".." , "directory" : "" } )        
+    for dirname in dirnames:
+            directories.append( {"title" : dirname , "directory" : [path + "/" + dirname] } )
     for filename in filenames:
         if filename.endswith(".mp4") or filename.endswith(".mov") or filename.endswith(".mkv"):
             videos.append( {"title" : filename , "sources" : [BASE_PATH + "/" + filename] } )
     break
     
-data = { "videos" : videos }
+data = { "videos" : videos , "directories" : directories}
 
 print(json.dumps(data))
 print("")
