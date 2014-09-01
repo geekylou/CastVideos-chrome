fb3560f3b0d4e437284f0334d13fe92e34cc4ae4
Louise Newberry <nn6215@googlemail.com>
2014-09-01 18:35:43 +0100

    It turns out the directory lists need sorting on some Linux (and probably other) machines.

--- getDirectoryListing.py
+++ getDirectoryListing.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-# enable debugging
-
-# The base path where your content is.  This should be set to '.' if content is in the same directory as the script.
-BASE_PATH="../videos"
-BASE_URL ="http://192.168.0.79/video/videos/"
 import os
 import cgitb
 import cgi
 import json
 cgitb.enable()
+
+# The base path where your content is.  This should be set to '.' if content is in the same directory as the script.
+BASE_PATH="../"
+
+# And this is the URL needed to access the same path on your web server.
+BASE_URL ="http://"+os.environ['SERVER_NAME']+"/video/"
+
+#cgi.print_environ()
+
+directory_arg = os.environ['REQUEST_URI'][len(os.environ['SCRIPT_NAME']):]
 
 print("Content-Type: application/json")
 print("")
@@ -39,12 +45,18 @@ for (dirpath, dirnames, filenames) in os.walk(path):
             directories.append( {"title" : ".." , "directory" : "" } )        
     else:
         base_arg = ""
+    
+    dirnames.sort()
         
     for dirname in dirnames:
             directories.append( {"title" : dirname , "directory" : [base_arg + dirname] } )
+            
+    filenames.sort()
+    
     for filename in filenames:
         if filename.endswith(".mp4") or filename.endswith(".mov") or filename.endswith(".mkv"):
-            videos.append( {"title" : filename , "sources" : [BASE_URL + path + "/" + filename] } )
+            videos.append( {"title" : filename , "sources" : [BASE_URL + base_arg + "/" + filename] } )
+    # Break here as we are only interested in the top level directory.
     break
     
 data = { "videos" : videos , "directories" : directories}
